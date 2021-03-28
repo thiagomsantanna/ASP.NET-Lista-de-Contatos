6f9b4c620597f99020256eda23f05b41e257cc25
Thiago Mundici <thiagomsantanna11@gmail.com>
2021-03-28 19:03:37 -0300

    Criação dos arquivos para CRUD via Scaffolding.

---
+++ Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneBook_DrinkCompany.Models
+{
+    public partial class Clientes
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Cpnj { get; set; }
+        public string Email { get; set; }
+        public string TelefoneCelular { get; set; }
+        public string TelefoneResidencial { get; set; }
+        public string TelefoneComercial { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Models/PhoneBookContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PhoneBook_DrinkCompany.Models
+{
+    public partial class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext()
+        {
+        }
+
+        public PhoneBookContext(DbContextOptions<PhoneBookContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Clientes> Clientes { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Clientes>(entity =>
+            {
+                entity.HasKey(e => e.ClienteId)
+                    .HasName("PK__Clientes__71ABD08798A3971C");
+
+                entity.Property(e => e.Cpf)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Cpnj)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Endereco)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TelefoneCelular)
+                    .IsRequired()
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TelefoneComercial)
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TelefoneResidencial)
+                    .HasMaxLength(45)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
