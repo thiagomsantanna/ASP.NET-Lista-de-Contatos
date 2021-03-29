272b4454945027848b5205635c16000f4bf85db5
Thiago Mundici <thiagomsantanna11@gmail.com>
2021-03-28 21:51:23 -0300

    Correção

--- Startup.cs
+++ Startup.cs
@@ -49,7 +49,7 @@ namespace PhoneBook_DrinkCompany
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Clientes}/{action=Index}/{id?}");
             });
         }
     }
